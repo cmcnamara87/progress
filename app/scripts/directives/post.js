20be8ee8d8cfa11a978a4fbef874629bca459b62
Craig McNamara <cmcnamara87@gmail.cok>
2014-07-10 07:54:49 +1000

    Move post code to separate directive

---
+++ app/scripts/directives/post.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('progressClientApp')
+  .directive('post', function () {
+    return {
+      templateUrl: 'views/post.html',
+      restrict: 'E',
+      link: function postLink() {
+        // element.text('this is the post directive');
+      }
+    };
+  });

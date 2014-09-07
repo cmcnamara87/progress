9599a3ecb6d75502e45e9d501ea43ea847865d1e
Craig McNamara <cmcnamara87@gmail.cok>
2014-09-07 21:58:16 +1000

    style(diary): fix some spacing in js file

--- app/scripts/controllers/diary.js
+++ app/scripts/controllers/diary.js
@@ -7,13 +7,11 @@ angular.module('progressClientApp')
         vm.project = project;
         vm.posts = posts;
 
-        // _.each(posts, function(post) {
-            _.each(project.directories, function(directory) {
-                console.log('directories');
-                var parts = directory.path.split('/');
-                directory.folderName = parts[parts.length - 1];
-            });
-        // });
+        _.each(project.directories, function(directory) {
+            console.log('directories');
+            var parts = directory.path.split('/');
+            directory.folderName = parts[parts.length - 1];
+        });
 
         // Only screenshots
         vm.posts = _.filter(posts, function(post) {
@@ -24,16 +22,13 @@ angular.module('progressClientApp')
             post.date = $filter('date')(post.created * 1000, 'fullDate');
         });
 
-		var dates = _.groupBy(vm.posts, 'date');
-		dates = _.map(dates, function(date) {
-			return {
-				date: date[0].date,
-				created: date[0].created,
-				entries: date
-			};
-		});
-		vm.dates = _.sortBy(dates, 'created').reverse();
-
-
-        User.getLoggedIn();
+        var dates = _.groupBy(vm.posts, 'date');
+        dates = _.map(dates, function(date) {
+            return {
+                date: date[0].date,
+                created: date[0].created,
+                entries: date
+            };
+        });
+        vm.dates = _.sortBy(dates, 'created').reverse();
     });

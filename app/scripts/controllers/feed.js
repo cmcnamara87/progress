db42433b819ee538881331f115b33c6785229275
Craig McNamara <cmcnamara87@gmail.cok>
2014-09-07 23:51:56 +1000

    feat(feed): add offline users
    Offline users now appear under online users on the feed page.

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -9,8 +9,24 @@ angular.module('progressClientApp')
         ];
 
 		console.log('hello world?');
+        var vm = $scope;
         
         // $scope.cats = Restangular.one('me').all('following').all('posts').getList();
 		$scope.posts = Post.getFeed();
-        $scope.online = User.getOnline();
+        User.getOnline().then(function(onlineUsers) {
+            $scope.online = onlineUsers;
+            console.log('got onlie');
+            // Get offline
+            Restangular.one('me').all('following').getList().then(function(allUsers) {
+                // console.log('getting offliem', allUsers);
+                // vm.offline = allUsers;
+                vm.offline = _.reject(allUsers, function(user) {
+                    return _.find(onlineUsers, function(yo) {
+                        return yo.id === user.id;
+                    });
+                    // return false;
+                    // return _.find(onlineUsers, {id: user.id});
+                });
+            });
+        });
     });

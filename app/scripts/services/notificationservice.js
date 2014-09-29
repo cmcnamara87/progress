cdd370e5e0f64e2373e8a9f6f750ad71b0a4a7b7
Craig McNamara <cmcnamara87@gmail.cok>
2014-09-29 22:14:31 +1000

    feat(notifications): add mark all as read

--- app/scripts/services/notificationservice.js
+++ app/scripts/services/notificationservice.js
@@ -4,13 +4,20 @@ angular.module('progressClientApp')
     .factory('notificationService', function(Restangular, Notification) {
         var service = {
             notifications: [],
-            loadNotifications: loadNotifications
+            loadNotifications: loadNotifications,
+            markAllAsRead: markAllAsRead
         };
         return service;
 
         function loadNotifications() {
             Notification.getNotifications().then(function(notifications) {
                 service.notifications = notifications;
+            });
+        }
+
+        function markAllAsRead() {
+            _.each(service.notifications, function(notification) {
+                notification.markAsRead();
             });
         }
     });
